cf220a296ac22d67fe882b03b98f9d4e3789162d
IvairGarcia <92556120+IvairGarcia@users.noreply.github.com>
2023-03-22 21:05:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,17 @@
-<!--
-Desenvolvido por Designer Lucas
-www.designerlucas.net
--->
-<!doctype html>
-<html>
+<?php
 
-<head>
-<meta charset="utf-8">
-<title>Calculadora IMC</title>
-<link href="enc/estilo.css" rel="stylesheet" type="text/css">
-</head>
+//Codigo Criado por: Ivair Rodrigues Garcia
 
-<body>
-<!--Content-->
-<div id="content">
-    <!--Logo-->
-    <div id="logo">
-    </div>
-    <!--Fim Logo-->
-    
-    <!--Calculadora-->
-    <div id="calculadora">
-    	<!--Formulario-->
-        <form name="calc" method="get" enctype="multipart/form-data" action="resultado.php" class="form">
-        	
-            <fieldset class="f">
-            	
-                <label>
-                <span class="span">Seu Peso</span>
-                <br/>
-                <input type="text" name="peso" class="in" />
-                </label>
-                
-                <br />
-                <label>
-                <span class="span">Sua Altura (Use Pontuação: 1.99 Em METROS)</span>
-                <br/>
-                <input type="text" name="altura" class="in"/>
-                </label>
-                
-                <input type="submit" name="envia" value="IMC!" class="btn">
-            
-            </fieldset>
-        
-        </form>
-        <!--Fim Formulario-->
-    </div>
-    <!--Fim Calculadora-->
-</div>
-<!--Fim Content-->   
-    
-</body>
-</html>
+$peso = $_POST['peso'];
+$altura = $_POST['altura'];
+
+function imc($altura, $peso){
+$altura = str_replace(',', '.', $altura);
+$result = $peso/$altura**2;
+return $result;
+
+}
+
+echo"seu imc é: " .imc($altura , $peso);
+
+?>
